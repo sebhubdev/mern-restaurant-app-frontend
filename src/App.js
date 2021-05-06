a9b3d2e30ed99e7daeb052fde3e6ea50535bf53c
sebaneumann <neumann.tecnica@gmail.com>
2021-05-06 12:46:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import AddReview from './components/add-review.js'
+import Login from './components/login.js'
+import Restaurant from './components/restaurant.js'
+import RestaurantsList from './components/restaurants-list.js'
+
+function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user=null)
+  {
+      setUser(user);
+  }
+
+  async function logout()
+  {
+      setUser(null);
+  }
+
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+            Restaurants Reviews
+        </a>
+        <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/restaurants"} className="nav-link">
+                Restaurants
+              </Link>
+            </li>
+            <li className="nav-item">
+              {
+              user ? 
+                  (
+                    <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+                      Logout {user.name}
+                    </a>
+                  ) 
+                  : 
+                  (
+                      <Link to={"/login"} className="nav-link">
+                        Login
+                      </Link>
+                  )
+              }
+            </li>
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/","/restaurants"]} component={RestaurantsList}/>
+          <Route 
+            path="/restaurants/:id/review" 
+            render={(props)=>{            
+            return <AddReview {...props} user={user}/>;
+          }}/>
+          <Route 
+            path="/restaurants/:id" 
+            render={(props)=>{            
+            return <Restaurant {...props} user={user}/>;
+          }}/>
+          <Route 
+            path="/login" 
+            render={(props)=>{            
+            return <Login {...props} login={login}/>;
+          }}/>
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/restaurant.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import RestaurantDataService from '../services/restaurant.js'
+import {Link} from 'react-router-dom' 
+
+function Restaurant() {
+  return (
+    <div className="App">
+        Restaurant
+    </div>
+  );
+}
+
+export default Restaurant;
